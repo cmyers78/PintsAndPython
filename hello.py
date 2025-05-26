5e5c39780c424ab6c91ad03e8d61d704318d48b8
Chris Myers <cmyers78@gmail.com>
2025-05-26 09:36:13 -0400

    Initial commit. lesson one and review files

---
+++ hello.py
@@ -0,0 +1,32 @@
+print("\nHello, World\n")
+
+# this is a comment it will not be read
+# These are examples of data types:
+name = "Christopher Michael Myers OF ATHENS" #string 
+age = 46 # integer
+pi = 3.14 # float 
+isSingle = False  #Boolean 
+
+# These are example of collections
+students = ["Dan", "Cyndi", "Erich", "Addah", "Squid", "Sofia", "Trevor", "Snook"]
+studentErich = students[2]
+studentCyndi = students[1]
+studentDan = students[0]
+studentAddie = students[3]
+
+print(len(students)) # this will tell me the length (number of values) in the list 
+
+print(students)
+"""
+print(studentErich)
+print(studentAddie)
+print(studentDan)
+print(studentCyndi)
+print()
+#print(f"My name is: {name} and I am {age} and i am single: {isSingle}")
+"""
+
+
+questionOfTheDay = input("Who is the best dog? ")
+print(f"{questionOfTheDay} is the best dog. ")
+
---
+++ review_1.py
@@ -0,0 +1,34 @@
+#Section 1 Answers
+
+#1
+print("Hello, world!")
+#2
+print("My favorite food is pizza")
+#3
+print("Today is a great day to learn Python")
+#4
+print("5 + 3 = 8")
+
+#Section 2 Answers
+#1
+food = "pizza"
+print(f"My favorite food is {food}")
+#2
+day = "Tuesday"
+print(f"Today is {day}")
+#3
+number = 7
+print(f"The number is {7}")
+#4
+a = 3
+b = 4
+print(f"The sum of {a} and {b} is {a+b}")
+#5
+x, y = 10, 2
+print(f"{y} minus {x} equals {y - x}")
+#6
+x, y = 5, 3
+print(f"{x} times {y} equals {x*y}")
+#7
+x, y = 20, 4
+print(f"{x} divided by {y} equals {x / 4}")
